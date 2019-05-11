c7a1f40bae238634fa03ad14d1bf06523003a237
Mirko Nitschke <mnitscke93@gmail.com>
2019-05-11 11:28:56 +0200

    Create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
-Rails.application.routes.draw do
+# frozen_string_literal: true
 
+Rails.application.routes.draw do
   root 'pages#homepage'
   get 'about', to: 'pages#about'
-  
+
+  resources :articles
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
